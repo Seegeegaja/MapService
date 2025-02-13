cdc41f3b076bab6295facfd764358a1f3533dcb1
seegeegaja <wnsdyd821@gmail.com>
2025-02-13 14:52:38 +0900

    'service'

--- src/main/java/com/my/mapService/service/MemberService.java
+++ src/main/java/com/my/mapService/service/MemberService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,32 +25,42 @@ public class MemberService {
     public MemberService(MapMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     //회원 가입기능
     public Long join(MemberDto memberDto) {
         //같은 이름이 있는 중복 회원은 XXXX
         Optional<MemberDto> result = memberRepository
                 .findByName(memberDto.getName());
         Optional<MemberDto> sum = memberRepository.findByName(memberDto.getAddress());
-
-        if (result.isPresent()) {
-
+        //이름 주소 둘다 확인
+        if (result.isPresent() && result.get().getAddress().equals(memberDto.getAddress())) {
             //true가 걸리면 같은이름이 존재
             return -1L;
-        } else{
-            if (sum.isPresent()) {
-                return -1L;
-            } else {
-                MemberDto save = memberRepository.save(memberDto);
-                return save.getId();
-            }
+        } else {
+            MemberDto save = memberRepository.save(memberDto);
+            return save.getId();
             //입력 가능
 //            MemberDto save = memberRepository.save(memberDto);
 //            return save.getId();
         }
     }
+
     //아이디로 검색하서 한개 찾기
     public Optional<MemberDto> findOne(Long id) {
         return memberRepository.findById(id);
     }
+    //전체 리스트 출력
+    public List<MemberDto> findAll() {
+        return memberRepository.findAll();
+    }
 
+    //삭제처리
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    //업데이트
+    public void update(MemberDto memberDto) {
+        memberRepository.updateById(memberDto.getId() , memberDto);
+    }
 }
--- src/test/java/com/my/mapService/service/MemberServiceTest.java
+++ src/test/java/com/my/mapService/service/MemberServiceTest.java
@@ -4,8 +4,10 @@ import com.my.mapService.dto.MemberDto;
 import com.my.mapService.repository.MapMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -47,7 +49,7 @@ class MemberServiceTest {
         //When
         MemberDto car = new MemberDto();
         car.setName("카리나");
-        car.setAddress("뉴욕");
+        car.setAddress("몰디브");
         Long chill = memberService.join(car);
         //then
         assertThat(chill).isEqualTo(-1L);
@@ -64,9 +66,61 @@ class MemberServiceTest {
         Long findId = memberService.join(kar);
 
         Optional<MemberDto> one = memberService.findOne(1L);
-        MemberDto qwe = memberService.findOne(findId).orElse(null);
-        assertThat(one.get().getName()).isEqualTo(kar.getName());
 
+        MemberDto qwe = memberService.findOne(findId).orElse(null);
+        assertThat(one.get().getName()).isEqualTo("카리나");
+        assertThat(one.get().getAddress()).isEqualTo("몰디브");
+
+    }
+    //전체 리스트 출력
+    @Test
+    @DisplayName("전체 리스트 검색")
+    public void findAll(){
+        MemberDto kar = new MemberDto();
+        kar.setName("카리나");
+        kar.setAddress("몰디브");
+        memberService.join(kar);
+        MemberDto win = new MemberDto();
+        win.setName("윈터");
+        win.setAddress("제주");
+        memberService.join(win);
+        //When
+        List<MemberDto> memberDtoList = memberService.findAll();
+//        Then
+        assertThat(memberDtoList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("아이디 받아서 삭제")
+    public void deleteById() {
+        MemberDto kar = new MemberDto();
+        kar.setName("카리나");
+        kar.setAddress("몰디브");
+        memberService.join(kar);
+
+        memberService.deleteById(1L);
+
+        int size = memberService.findAll().size();
+        assertThat(size).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("업데이트")
+    public void update() {
+        MemberDto kar = new MemberDto();
+        kar.setName("카리나");
+        kar.setAddress("몰디브");
+        memberService.join(kar);
+        MemberDto win = new MemberDto();
+        win.setId(1L);
+        win.setName("윈터");
+        win.setAddress("제주");
+
+        memberService.update(win);
+        MemberDto result = memberService.findOne(1L).orElse(null);
+
+        assertThat(result.getName()).isEqualTo("윈터");
+        assertThat(result.getAddress()).isEqualTo("제주");
     }
 
 }

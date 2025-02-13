32c756b8081cea68a31f6f587e7f104b000d82a0
seegeegaja <wnsdyd821@gmail.com>
2025-02-13 17:03:52 +0900

    '완성'

--- src/main/java/com/my/mapService/controller/MemberController.java
+++ src/main/java/com/my/mapService/controller/MemberController.java
@@ -1,25 +1,44 @@
 package com.my.mapService.controller;
 
 import com.my.mapService.dto.MemberDto;
+import com.my.mapService.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
+import java.util.List;
 
 @Controller
 @RequestMapping("/members")
+//@RequiredArgsConstructor
 public class MemberController {
+
+//    private final MemberService service;
+        private final MemberService service;
+
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
+
+
+//    @Autowired
+//    MemberService service;
+
     @GetMapping("")
-    public String user() {
+    public String user(Model model) {
         //전체 리스트 가져오기
+        List<MemberDto> memberList = service.findAll();
+        model.addAttribute("memberList",memberList);
         return "members/memberList";
     }
 
 
     @GetMapping("/new")
-    public String qwe() {
+    public String qwe(Model model) {
+        model.addAttribute("member", new MemberDto());
         //신규 회원 가입하기
         return "members/createMemberForm";
     }
@@ -32,10 +51,31 @@ public class MemberController {
 //    }
 
     @PostMapping("/new")
-    public String createMember(MemberDto memberDto) {
-        System.out.println(memberDto);
+    public String createMember( MemberDto memberDto) {
+        service.join(memberDto);
         return "redirect:/members";
     }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id")Long id, Model model) {
+        System.out.println("id : " +id);
+        service.deleteById(id);
+        return "redirect:/members";
+    }
+
+    @GetMapping("/update/{id}")
+    public String upDate(@PathVariable("id")Long id ,Model model) {
+        //1.아이디로 검색 후 member를 수정 폼에 전달 해서 뿌려줌
+        model.addAttribute("member", service.findOne(id));
+        return "members/updateMemberForm";
+    }
+    @PostMapping("/update")
+    public String update(MemberDto memberDto) {
+        System.out.println(memberDto);
+        service.update(memberDto);
+        return "redirect:/members";
+    }
+
+//    updateMemberForm
 //    @GetMapping("/")
 //    public String home(){
 //        return "home";
--- src/test/java/com/my/mapService/service/MemberServiceTest.java
+++ src/test/java/com/my/mapService/service/MemberServiceTest.java
@@ -3,7 +3,6 @@ package com.my.mapService.service;
 import com.my.mapService.dto.MemberDto;
 import com.my.mapService.repository.MapMemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
     MapMemberRepository repository = new MapMemberRepository();
